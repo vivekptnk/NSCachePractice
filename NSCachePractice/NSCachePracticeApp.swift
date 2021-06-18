5c11ae76ee314d4a97170668c0c991f98e14d28c
vivekptnk <vivekpattanaik100@gmail.com>
2021-06-18 17:59:39 -0400

    Initial Commit

---
+++ NSCachePractice/NSCachePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSCachePracticeApp.swift
+//  NSCachePractice
+//
+//  Created by Vivek Pattanaik on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct NSCachePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
